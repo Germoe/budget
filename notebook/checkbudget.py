Move Transaction class and fix calculation of total column

notebook/checkbudget.py
@@ -6,8 +6,6 @@ budget = Budget('../data/budget/budget_new.csv')
 budget.budget
 
 # +
-import glob, datetime, re
-import numpy as np
 import pandas as pd
 
 bank_configs = {
@@ -103,129 +101,12 @@ bank_configs = {
     }
 }
 
-class Transaction():
-    latest_historic_file = None
-    base_path='../data/transactions/'
-    source = None
-    transactions=None
-    
-    def __init__(self, source):
-        self.source = source
-        pass
-    
-    def add_transactions(self, f_path, name, at_total, config):
-        if config not in bank_configs:
-            raise ValueError(f'Not Existing Configuration: {config}')
-            
-        df = self._prepare_transactions(f_path, name, at_total, bank_configs[config])
-        self.transactions = self.merge_with_existing_data(df, name)
-        if self.transactions is None:
-            print(f'Reading in from: {name}')
-            self.transactions = self.read_transaction_data(name)
-            
-    def read_transaction_data(self, source):
-        self.source = source
-        historic_files = glob.glob(self.base_path + f'{source}-20*.csv')
-
-        if len(historic_files) == 0:
-            raise ValueError(f"No Transaction Data Available for {source}")
-
-        historic_files_max_idx = np.argmax([ re.search(r'(\d{4})-(\d{2})-(\d{2})-(\d{2})-(\d{2})',f).group() for f in historic_files])
-        self.latest_historic_file = historic_files[historic_files_max_idx]
-        latest_df = pd.read_csv(self.latest_historic_file, parse_dates=['transaction_date'], dtype={
-                                'amount':np.float32,
-                                'description': object,
-                                'beneficiary': object,
-                                'currency': object,
-                                'name': object,
-                                'total': np.float32,
-                                'label': object
-                            }).sort_values(['transaction_date','description','beneficiary','amount'],ascending=False)
-
-#         latest_df['source'] = source
-#         latest_df['fx_rate'] = add_fx_rate(latest_df)
-        return latest_df
-
-    def merge_with_existing_data(self, df, source):
-        historic_files = glob.glob(self.base_path + f'{source}*.csv')
-        new = df.sort_values(['transaction_date','description','beneficiary','amount'],ascending=False)
-        new['label'] = ''
-        upload_datetime = datetime.datetime.strftime(datetime.datetime.utcnow(),'%Y-%m-%d-%H-%M')
-
-        if len(historic_files) == 0:
-            print('No historic files. New File is written.')
-            df_new = new
-        else:
-            df = self.read_transaction_data(source)
-            latest_transaction = df['transaction_date'].max()
-            # TODO Improve merge procedure to avoid missing data
-            df = df.loc[df['transaction_date'] < latest_transaction,:].copy(deep=True)
-            new = new.loc[new['transaction_date'] >= latest_transaction,:]
-            df_new = pd.concat([new, df])
-            if df_new.equals(df):
-                print(f'Warning: No new transactions added. No Output written for {source}.')
-                return None
-            elif len(df_new) <= len(df):
-                print(f'Warning: Data has changed but new output is shorter or of equal length. No Output written for {source}.')
-                return None
-
-        df_new.to_csv(self.base_path + f'{source}-{upload_datetime}.csv', index=False)
-        return df_new
-
-    def _prepare_transactions(self, f_path, name, at_total, json_config):
-        if json_config['format'] == 'csv':
-            df = pd.read_csv(f_path, 
-                             delimiter=json_config['delimiter'], 
-                             skiprows=json_config['skiprows'], 
-                             encoding= 'unicode_escape')
-        elif json_config['format'] == 'xlsx':
-            df = pd.read_excel(f_path, 
-                               sheet_name=json_config['sheetname'], 
-                               skiprows=json_config['skiprows'], 
-                               engine="openpyxl")
-        
-        df = df.iloc[json_config['drop_rows']:,:]
-        
-        if "amount_map" in json_config:
-            df = combine_debit_credit(df, json_config['amount_map'], json_config['mapping'])
-
-        df = prepare_data(df, 
-                              json_config['mapping'], 
-                              dayfirst=json_config['dayfirst'], 
-                              currency=json_config['currency'], 
-                              name=name, 
-                              cent_delimiter=json_config['cent_delimiter'], 
-                              at_total=at_total)
-        return df
-    
-    
-    def update_total(self,at_total):
-        # Add total_at
-        upload_datetime = datetime.datetime.strftime(datetime.datetime.utcnow(),'%Y-%m-%d-%H-%M')
-        
-        # Calculate Beginning Balance
-        df_cln = self.transactions
-        
-        if (df_cln['transaction_date'] > at_total[0]).sum() > 0:
-            end_bal = df_cln.loc[df_cln['transaction_date'] > at_total[0],['transaction_date','amount']].sort_values('transaction_date', ascending=True)
-            end_bal['total'] = at_total[1] + end_bal['amount'].shift(0).cumsum()
-            end_bal = end_bal.iat[-1, 2]
-        else:
-            end_bal = at_total[1]
-
-        df_cln['total'] = end_bal - df_cln['amount'].shift(1).cumsum()
-        df_cln['total'].iat[0] = end_bal
-        
-        self.transactions = df_cln
-        self.transactions.to_csv(self.base_path + f'{self.source}-{upload_datetime}.csv', index=False)
-
-
 
 
 # +
 dkb_path = "../data/raw/dkb.csv"
 dkb_name = "dkb"
-dkb_at_total = ('2022-03-04', 2994.55)
+dkb_at_total = ('2022-04-03', 2994.55)
 dkb_config = 'dkb'
 
 dkb_credit_path = "../data/raw/dkb-credit.csv"